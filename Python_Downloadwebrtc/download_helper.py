57b63b4878f44d4fd7808a0699ebb4b8d10a1c53
tangpeng <tangpeng@bytedance.com>
2017-09-02 21:10:41 +0800

    Add script from webrtc's sourcecode

--- Python_Downloadwebrtc/download_helper.py
+++ Python_Downloadwebrtc/download_helper.py
@@ -20,23 +20,3 @@ def download_http_to_file(url, fileName):
                 break
             f.write(chunk)
     print ('download ......ok')
-
-
-if __name__ == "__main__":
-    f = open("./grep_download_error.log")
-    line = f.readline()
-    while line:
-        #print line,
-        line = f.readline()
-        if not line:
-            break
-        arr = line.split()
-        url = arr[5]
-        addr = arr[7]
-        fulladdr = "WebRTC_BuildScripts2/webrtc-build-scripts/android/webrtc/" + addr[0:-1]
-        print(url)
-        print(fulladdr)
-        download_gs_to_file(url, fulladdr)
-    f.close()
-    print ('This is main of module "hello.py"')
-    #download_gs_to_file('gs://chromium-android-tools/play-services/10.2.0/31843001b7ce94fbdf71f2a9db76b28548a795fa', '/tmp/tangpeng/f1')
